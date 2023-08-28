606739974679e9cc4f74d5557402e6ca200a4b42
uselessconsumer <eliottdale@gmail.com>
2023-08-28 18:12:32 -0400

    create function to generate the the number of divs in the grid

--- eas.js
+++ eas.js
@@ -1,16 +1,37 @@
-// create a 16x16 grid of divs (watch for borders and margins)
-    //greate a single 'square' using js, this will be used to generate all 16x16
-    //assign a class, and style the div using css
+//create a 16x16 grid
+//
 
 //put these divs into .gridContainer
 //make them appear as a grid using flexbox
 
+
+//FOR LATER:
+//calculate(function) the grid size: for now just do 16 by 16
+    //16 divs will span across and down
+    //determine what size the divs will scale to in order to fill the gridContainer
+
+
 const gridContainer = document.querySelector('div.gridContainer');
 
+//creates a div to be used as a node in the grid
 function createGridDiv(){
     let gridDiv = document.createElement('div');
     gridDiv.classList.add('gridDiv');
     gridContainer.appendChild(gridDiv);
 }
 
-createGridDiv();
+fillGrid();
+
+
+
+function fillGrid(gridSize) {
+    gridSize = 3;
+    let totalDiv = Math.pow(gridSize, 2);
+    let divCount = 0;
+    do {
+        createGridDiv();
+        ++divCount;
+    } while (divCount < totalDiv);
+}
+
+
