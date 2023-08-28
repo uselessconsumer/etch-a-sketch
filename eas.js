e7e109c7524150eaade4f4a05070db45240dac28
uselessconsumer <eliottdale@gmail.com>
2023-08-28 16:47:54 -0400

    create div to contain game and select them in js

---
+++ eas.js
@@ -0,0 +1,8 @@
+// create a 16x16 grid of divs (watch for borders and margins)
+    //greate a single 'square' using js, this will be used to generate all 16x16
+    //assign a class, and style the div using css
+
+//put these divs into .gridContainer
+//make them appear as a grid using flexbox
+
+const gridContainer = document.querySelector(div.gridContainer);
