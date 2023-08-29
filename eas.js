6f589f2df8adc14162e41548f527bd2428abe4c4
uselessconsumer <eliottdale@gmail.com>
2023-08-29 16:31:40 -0400

    create events to change the background colour of the div's

--- eas.js
+++ eas.js
@@ -3,6 +3,10 @@
     //or using JavaScript to change the divs background.
 
 
+//change the bg color of the div when clicked, or when the mouse is being clicked and is dragged over a div
+    //
+
+
 const gridContainer = document.querySelector('div.gridContainer');
 let gridDiv;
 let gridSize = 16;
@@ -32,5 +36,19 @@ function fillGrid() {
 
 fillGrid();
 
+//creates a toggled to determine if the mouse is being held down
+let toggle = false;
+document.addEventListener('mousedown', () => toggle = true);
+document.addEventListener('mouseup', () => toggle = false);
+
+//selects all divs in the grid
 const allGridDiv = document.querySelectorAll('.gridDiv');
-console.log(allGridDiv);
+allGridDiv.forEach((div) => {
+    div.addEventListener('mousedown', () => div.style.backgroundColor = 'black');
+    div.addEventListener('mouseover', () => {
+        if (toggle === true){
+            div.style.backgroundColor = 'black';
+        };
+    });
+});
+
